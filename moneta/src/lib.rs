6f60c756fa4d0f28f9bd0e8ca4bb7f37881d508f
Defmc <defmc0101@gmail.com>
2022-12-13 19:11:57 -0300

    fix: isolate wrappers

--- moneta/src/lib.rs
+++ moneta/src/lib.rs
@@ -135,19 +135,20 @@ pub fn moneta(meta: TokenStream, input: TokenStream) -> TokenStream {
             )
         })
         .collect();
-    let name = def_fn.sig.ident.clone();
+
+    let wrapper_name = def_fn.sig.ident.clone();
     let out_args: Vec<_> = args.iter().map(|(_, arg, _)| arg).collect();
-    let def_args: Vec<_> = args.iter().map(|(name, _, _)| name).collect();
-    let func_name = LitStr::new(&format!("{name}"), Span::call_site());
+    let func_name = LitStr::new(&format!("{}", outter.sig.ident), Span::call_site());
     let res_id = Ident::new("res", Span::call_site());
     let start_id = Ident::new("start", Span::call_site());
+    let values_id = Ident::new("values_fmt", Span::call_site());
 
     let get_ret = if options.cache.is_enabled(cfg!(feature = "cache")) {
         quote! {
-            let #res_id = #name (#(#out_args),*);
+            let #res_id = #wrapper_name (#(#out_args),*);
         }
     } else {
-        quote! { let #res_id = #name (#(#out_args),*); }
+        quote! { let #res_id = #wrapper_name (#(#out_args),*); }
     };
 
     let (trace_in, trace_out) = trace(
@@ -156,20 +157,18 @@ pub fn moneta(meta: TokenStream, input: TokenStream) -> TokenStream {
         &func_name,
         &start_id,
         args_lit_name.iter(),
-        def_args.iter().copied(),
+        out_args.iter().copied(),
     );
+    let let_values_fmt = let_values_fmt(&values_id, &out_args);
     let cache_def = cache_def(&cache_id, &vis, &ret_ty);
-    let (cache_get, cache_set) = cache(&options.cache, &def_args, &out_args, &cache_id, &res_id);
+    let (cache_get, cache_set) = cache(&options.cache, &values_id, &cache_id, &res_id);
     let (counter_def, counter_inc) = counter(&options.count, &vis, &counter_id);
 
-    let pre_injection = quote! {{
+    let post_injection = quote! {{
         #counter_inc
+        #let_values_fmt
         #trace_in
         #cache_get
-    }}
-    .into();
-
-    let post_injection = quote! {{
         let #start_id = std::time::Instant::now();
         #get_ret
         #trace_out
@@ -177,11 +176,6 @@ pub fn moneta(meta: TokenStream, input: TokenStream) -> TokenStream {
         return #res_id;
     }}
     .into();
-
-    def_fn
-        .block
-        .stmts
-        .insert(0, parse_macro_input!(pre_injection as Stmt));
 
     outter.block.stmts = vec![
         Stmt::Item(Item::Fn(def_fn)),
@@ -212,7 +206,7 @@ fn trace<'a>(
     name_str: &LitStr,
     start_id: &Ident,
     args_names: impl Iterator<Item = &'a LitStr>,
-    def_args: impl Iterator<Item = &'a Pat>,
+    out_args: impl Iterator<Item = &'a Ident>,
 ) -> (TokenStream2, TokenStream2) {
     let trace_enabled = trace_enabled.is_enabled(cfg!(feature = "trace"));
 
@@ -221,7 +215,7 @@ fn trace<'a>(
             let args_fmt: String = [
                 #(#args_names,)*
             ].into_iter()
-                .zip([#(format!("{:?}", #def_args),)*].into_iter())
+                .zip([#(format!("{:?}", #out_args),)*].into_iter())
                 .map(|(n, v): (&str, String)| format!("\n\t{}: {}", n, v))
                 .collect();
             println!("in {}: {}", #name_str, args_fmt);
@@ -247,19 +241,16 @@ fn trace<'a>(
 
 fn cache(
     enabled: &Opt,
-    def_args: &Vec<&Pat>,
-    out_args: &Vec<&Ident>,
+    values_fmt: &Ident,
     counter_id: &Ident,
     res_id: &Ident,
 ) -> (TokenStream2, TokenStream2) {
-    let debug_fmt = LitStr::new(&"{:?}".repeat(def_args.len()), Span::call_site());
     let enabled = enabled.is_enabled(cfg!(feature = "cache"));
 
     let get_cache = if enabled {
         quote! {{
-            let values_fmt = format!(#debug_fmt, #(#def_args),*);
             if let Ok(reader) = #counter_id.read() {
-                if let Some(val) = reader.get(&values_fmt) {
+                if let Some(val) = reader.get(&#values_fmt) {
                     return val.clone();
                 }
             }
@@ -270,9 +261,8 @@ fn cache(
 
     let set_cache = if enabled {
         quote! {{
-            let values_fmt = format!(#debug_fmt, #(#out_args),*);
             if let Ok(mut writer) = #counter_id.write() {
-                writer.entry(values_fmt).or_insert(#res_id.clone());
+                writer.entry(#values_fmt).or_insert(#res_id.clone());
             }
         }}
     } else {
@@ -294,4 +284,11 @@ fn counter(enabled: &Opt, vis: &TokenStream2, counter_id: &Ident) -> (TokenStrea
     };
 
     (def, inc)
+}
+
+fn let_values_fmt(id: &Ident, args: &Vec<&Ident>) -> TokenStream2 {
+    let debug_fmt = LitStr::new(&"{:?}".repeat(args.len()), Span::call_site());
+    quote! {
+        let #id = format!(#debug_fmt, #(#args),*);
+    }
 }
