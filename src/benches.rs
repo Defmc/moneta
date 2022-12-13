d69655645f1abb672972ead5aa399d45063c7546
Defmc <defmc0101@gmail.com>
2022-12-13 11:09:26 -0300

    feat: use a stable benchmark library

---
+++ src/benches.rs
@@ -0,0 +1,12 @@
+use bencher::{benchmark_group, benchmark_main, black_box, Bencher};
+
+fn fibonacci_50(b: &mut Bencher) {
+    b.iter(|| assert_eq!(black_box(moneta_bin::fibonacci(10)), 89))
+}
+
+fn tribonacci_10(b: &mut Bencher) {
+    b.iter(|| assert_eq!(black_box(moneta_bin::tribonacci(10)), 149))
+}
+
+benchmark_group!(num_seqs, fibonacci_50, tribonacci_10);
+benchmark_main!(num_seqs);
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,26 @@
-#![feature(test)]
+use moneta_fn::moneta;
+
+#[moneta]
+pub fn fibonacci(n: u128) -> u128 {
+    match n {
+        0 | 1 => 1,
+        _ => (1..=2).map(|o| fibonacci(n - o)).sum(),
+    }
+}
+
+#[moneta]
+pub fn tribonacci(n: u128) -> u128 {
+    match n {
+        0 => 0,
+        1 | 2 => 1,
+        3 => 2,
+        _ => (1..=3).map(|o| tribonacci(n - o)).sum(),
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use moneta_fn::{count, get_cache, moneta};
-    extern crate test;
-    use test::{black_box, Bencher};
-
-    #[moneta]
-    fn fibonacci(n: u128) -> u128 {
-        match n {
-            0 | 1 => 1,
-            _ => (1..=2).map(|o| fibonacci(n - o)).sum(),
-        }
-    }
-
-    #[moneta]
-    fn tribonacci(n: u128) -> u128 {
-        match n {
-            0 => 0,
-            1 | 2 => 1,
-            3 => 2,
-            _ => (1..=3).map(|o| tribonacci(n - o)).sum(),
-        }
-    }
 
     #[test]
     fn multiple_call() {
@@ -40,16 +38,6 @@ mod tests {
         fns::baz();
         assert_eq!(count!(foo), 1);
         assert_eq!(count!(fns::baz), 2);
-    }
-
-    #[bench]
-    fn fibonacci_50(b: &mut Bencher) {
-        b.iter(|| assert_eq!(black_box(fibonacci(10)), 89))
-    }
-
-    #[bench]
-    fn tribonacci_10(b: &mut Bencher) {
-        b.iter(|| assert_eq!(black_box(tribonacci(10)), 149))
     }
 
     #[test]
