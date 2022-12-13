6109506c8cb835c8d93097e967c791ed73791b24
Defmc <defmc0101@gmail.com>
2022-12-13 11:50:04 -0300

    fix: use black_box in the function input

--- src/benches.rs
+++ src/benches.rs
@@ -1,11 +1,11 @@
 use bencher::{benchmark_group, benchmark_main, black_box, Bencher};
 
 fn fibonacci_50(b: &mut Bencher) {
-    b.iter(|| assert_eq!(black_box(moneta_bin::fibonacci(10)), 89))
+    b.iter(|| assert_eq!(moneta_bin::fibonacci(black_box(10)), 89))
 }
 
 fn tribonacci_10(b: &mut Bencher) {
-    b.iter(|| assert_eq!(black_box(moneta_bin::tribonacci(10)), 149))
+    b.iter(|| assert_eq!(moneta_bin::tribonacci(black_box(10)), 149))
 }
 
 benchmark_group!(num_seqs, fibonacci_50, tribonacci_10);
